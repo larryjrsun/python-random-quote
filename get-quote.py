968053cb9faf6880a70c93213ecadae14ffb8fbd
JINGRAN SUN <jrsun@JINGRANs-MacBook-Pro.local>
2021-01-31 11:04:41 -0800

    renamed the primary function

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,36 @@
-def main():
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+def primary():
   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
 
-  #print(quotes)
+# In[6]:
+
+
+#f = open("quotes.txt")
+#quotes = f.readlines()
+#f.close()
+
+
+# In[7]:
+
+
+#print(quotes)
+
+
+# In[11]:
+
 
 if __name__== "__main__":
-  main()
+  primary()
+
+
+# In[ ]:
+
+
+
+
